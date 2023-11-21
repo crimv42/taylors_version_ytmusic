c25cd96b91cb5e2513c362836805fe395808645f
Daniel Nelems <daniel.nelems@gmail.com>
2023-11-21 00:02:11 -0700

    progress save

--- main/main.py
+++ main/main.py
@@ -1,59 +1,68 @@
 from ytmusicapi import YTMusic
 import json
+import re
 ytmusic = YTMusic("oauth.json")
 
 stolen_tracks_lists = []
 playlist_name = ""
 
-# Map Structure {"track_name": ["stolenId", "tvId"]}
-map_fearless = {}
-map_red = {}
-map_speakNow = {}
-map_1989 = {}
-map_1989_DLX = {}
-
-#### Get Album Info
-
-## Fearless
-# Fearless Stolen
-stolen_fearless = ytmusic.get_album("MPREb_WOymlv5h6Sg")
-# Fearless Platinum Stolen
-stolen_fearless_plat = ytmusic.get_album("MPREb_T8VII3ez1rZ")
-
-
-## Speak Now
-
-## Red
-
-## 1989
-# 1989 TV
-tv_1989 = ytmusic.get_album("MPREb_5XXbjpMgJar")
-# 1989 Stolen
-stolen_1989 = ytmusic.get_album("MPREb_K9GL6DCDUCq")
-
-# 1989 TV DLX Info
-# 1989 TV Album ID = MPREb_5XXbjpMgJar
-tv_1989_DLX = ytmusic.get_album("MPREb_ROOaFgqZpLS")
-
-# 1989 Stolen DLX Info
-# 1989 Stolen Album ID = MPREb_riIJjrsqo2I
-stolen_1989_DLX = ytmusic.get_album("MPREb_riIJjrsqo2I")
-
-
-#### End Get Album Info
 
 ## Playlist info
 playlist_list = ytmusic.get_library_playlists()
 
+albumNames = ["fearless", "speakNow", "red", "nineteenEightyNine"]
+tvAlbums = {
+    "fearless": ["OLAK5uy_mGRDKgRDJtrpyw25zrYD7Rl56ACL1Oiy8"],
+    "speakNow": ["OLAK5uy_ne0DpedKNeKeFyjQ86_6DJHyCGCjqHOv8"],
+    "red": ["OLAK5uy_l1Em5x2MlWyFc1w_7ayxQc6qVLVUbhaME"],
+    "nineteenEightyNine": ["OLAK5uy_ncKvykObOe16WxrmZEAlCbWBRZm2Utaqk", "OLAK5uy_lvlz2hg23jGszL0Jaa9Wy9TE_XNnfMijM"]
+}
+stolenAlbums = {
+    "fearless": ["OLAK5uy_muzVJZB508O6Pn7hAnKLX-0FhxVFC9Z9M", "OLAK5uy_lZdZBuYMGZcc5AJZmJeGN-390ORcsEtJU", "OLAK5uy_k9QEkm7Med3kLvYpQVSJXb_kSKyBOi8BE"],
+    "speakNow": ["OLAK5uy_mDY0SYhu9rF7RdxJNVsJHTxSeKhD6DxVQ", "OLAK5uy_ma7jNM1HuRvhHdcqHGtyYWuB_r-_hqN-c"],
+    "red": ["OLAK5uy_ks84GLjEXPo9_C4dbTmyX99z5W0gwzFIk", "OLAK5uy_mnq56DGqyvOiaqJtjHxlksOHOJhKRCseU"],
+    "nineteenEightyNine": ["OLAK5uy_n8mGmmk-aCPt9QQ9dOLQAHj0fDa7bBuLA", "OLAK5uy_mWbyIY5FoCOlKwTI9xXLoRXzv3VpoYv_g", "OLAK5uy_knugp3pcdtuz6Oil8vBHX9V5PkpVCnnK0"]
+}
+albumDict = {album: {"tvAlbums": tvAlbums[album], "stolenAlbums": stolenAlbums[album]} for album in albumNames}
 
 #### Functions
-def map_maker(albumMap, stolenTracks, tvTracks):
-    # mapName = ("map_" + albumName)
-    for trackNum in range(len(stolenTracks["tracks"])):
-        trackName = stolenTracks["tracks"][trackNum]["title"]
-        if not "(Voice Memo)" in trackName and trackName in tvTracks["tracks"][trackNum]["title"]:
-            stolenId = stolenTracks["tracks"][trackNum]["videoId"]
-            albumMap[stolenId] = tvTracks["tracks"][trackNum]["videoId"]
+def getTrackNames(albumDict):
+    albumsDict = {}
+
+    for albumName in albumDict:
+        albumsDict[albumName] = {}
+
+        # Combined processing for both TV and stolen albums
+        for albumType in ["tvAlbums", "stolenAlbums"]:
+            for albumId in albumDict[albumName][albumType]:
+                search_results = ytmusic.get_album_browse_id(albumId)
+                albumData = ytmusic.get_album(search_results)
+
+                for track in albumData["tracks"]:
+                    trackTitle = track["title"]
+
+                    # Skip tracks with "Voice Memo" or "Commentary"
+                    if "Voice Memo" in trackTitle or "Commentary" in trackTitle:
+                        continue
+
+                    if "From The Vault" not in trackTitle:
+                        # Clean and standardize the track title
+                        trackTitle = " ".join(re.findall(r"[a-zA-Z0-9\(\)\&]+", trackTitle))
+                        trackTitle = trackTitle.replace(" (Taylor s Version)", "").strip()
+
+                        # Initialize dictionary for the track if it does not exist
+                        if trackTitle not in albumsDict[albumName]:
+                            albumsDict[albumName][trackTitle] = {"stolenVideoIds": [], "tvVideoId": None}
+
+                        # Assign video ID based on album type
+                        videoIdKey = "tvVideoId" if albumType == "tvAlbums" else "stolenVideoIds"
+                        videoIdList = albumsDict[albumName][trackTitle][videoIdKey]
+                        if isinstance(videoIdList, list):
+                            videoIdList.append(track["videoId"])
+                        else:
+                            albumsDict[albumName][trackTitle][videoIdKey] = track["videoId"]
+
+    return albumsDict
 
 def replace_track(stolenTrackId, tvTrackId, stolenSetVideoId, playlistId):
     trackInfo = ytmusic.get_song(stolenTrackId)
@@ -86,24 +95,27 @@ def search_playlist(trackMap):
         current_playlist = ytmusic.get_playlist(playlist["playlistId"])
 
         for track in current_playlist["tracks"]:
-            if track["videoId"] in trackMap.keys():
+            if track["videoId"] in trackMap:
                 if "setVideoId" in track:
                     print(playlist["title"])
                     setVideoId = track["setVideoId"]
                     print(setVideoId)
   
                     stolenId = track["videoId"]
-                    replace_track(stolenId, trackMap[stolenId], setVideoId, playlistId)
+                    # replace_track(stolenId, trackMap[stolenId], setVideoId, playlistId)
 
 def main():
-    map_maker(map_1989, stolen_1989, tv_1989)
-    map_maker(map_1989_DLX, stolen_1989_DLX, tv_1989_DLX)
-    
+    fullAlbumDict = getTrackNames(albumDict)
+    for album in fullAlbumDict.keys():
+        for track in fullAlbumDict[album].keys():
+            stolenVideoIds = fullAlbumDict[album][track]["stolenVideoIds"]
+            tvId = fullAlbumDict[album][track]["tvVideoId"]
+            if stolenVideoIds != [] and tvId != "":
+                search_playlist(stolenVideoIds)
+                # print(track)
+                # print(stolenVideoIds)
+                # print(tvId)
 
-    search_playlist(map_1989)
-    search_playlist(map_1989_DLX)
-    # map_maker(map_1989_DLX, stolen_1989_DLX, tv_1989_DLX)
-    # search_playlist(map_1989_DLX)
 
 #### End Functions
 
--- main/search.py
+++ main/search.py
@@ -3,63 +3,83 @@ import json
 import re
 ytmusic = YTMusic("oauth.json")
 
-# search_results = ytmusic.search("Taylor Swift 1989")
-albumNames = ["fearless", "speakNow", "red", "1989"]
-fullAlbumDict = {album: {"tvAlbums": [], "stolenAlbums": []} for album in albumNames}
-
-
 # Fearless
 # TV: OLAK5uy_mGRDKgRDJtrpyw25zrYD7Rl56ACL1Oiy8
 # Regular: OLAK5uy_muzVJZB508O6Pn7hAnKLX-0FhxVFC9Z9M
 # Platinum Edition: OLAK5uy_lZdZBuYMGZcc5AJZmJeGN-390ORcsEtJU
 
 # Speak Now
+# TV: OLAK5uy_ne0DpedKNeKeFyjQ86_6DJHyCGCjqHOv8
+# Regular: OLAK5uy_mDY0SYhu9rF7RdxJNVsJHTxSeKhD6DxVQ
+# Deluxe: OLAK5uy_ma7jNM1HuRvhHdcqHGtyYWuB_r-_hqN-c
 
 # Red
+# TV: OLAK5uy_l1Em5x2MlWyFc1w_7ayxQc6qVLVUbhaME
+# Regular: OLAK5uy_ks84GLjEXPo9_C4dbTmyX99z5W0gwzFIk
+# BMR_Red: OLAK5uy_mnq56DGqyvOiaqJtjHxlksOHOJhKRCseU
 
 # 1989
+# TV: OLAK5uy_ncKvykObOe16WxrmZEAlCbWBRZm2Utaqk
+# Regular: OLAK5uy_n8mGmmk-aCPt9QQ9dOLQAHj0fDa7bBuLA
+# Deluxe: OLAK5uy_mWbyIY5FoCOlKwTI9xXLoRXzv3VpoYv_g
+# BMR: OLAK5uy_knugp3pcdtuz6Oil8vBHX9V5PkpVCnnK0
 
-search_results = ytmusic.get_album_browse_id("OLAK5uy_mGRDKgRDJtrpyw25zrYD7Rl56ACL1Oiy8")
+albumNames = ["fearless", "speakNow", "red", "nineteenEightyNine"]
+tvAlbums = {
+    "fearless": ["OLAK5uy_mGRDKgRDJtrpyw25zrYD7Rl56ACL1Oiy8"],
+    "speakNow": ["OLAK5uy_ne0DpedKNeKeFyjQ86_6DJHyCGCjqHOv8"],
+    "red": ["OLAK5uy_l1Em5x2MlWyFc1w_7ayxQc6qVLVUbhaME"],
+    "nineteenEightyNine": ["OLAK5uy_ncKvykObOe16WxrmZEAlCbWBRZm2Utaqk", "OLAK5uy_lvlz2hg23jGszL0Jaa9Wy9TE_XNnfMijM"]
+}
+stolenAlbums = {
+    "fearless": ["OLAK5uy_muzVJZB508O6Pn7hAnKLX-0FhxVFC9Z9M", "OLAK5uy_lZdZBuYMGZcc5AJZmJeGN-390ORcsEtJU", "OLAK5uy_k9QEkm7Med3kLvYpQVSJXb_kSKyBOi8BE"],
+    "speakNow": ["OLAK5uy_mDY0SYhu9rF7RdxJNVsJHTxSeKhD6DxVQ", "OLAK5uy_ma7jNM1HuRvhHdcqHGtyYWuB_r-_hqN-c"],
+    "red": ["OLAK5uy_ks84GLjEXPo9_C4dbTmyX99z5W0gwzFIk", "OLAK5uy_mnq56DGqyvOiaqJtjHxlksOHOJhKRCseU"],
+    "nineteenEightyNine": ["OLAK5uy_n8mGmmk-aCPt9QQ9dOLQAHj0fDa7bBuLA", "OLAK5uy_mWbyIY5FoCOlKwTI9xXLoRXzv3VpoYv_g", "OLAK5uy_knugp3pcdtuz6Oil8vBHX9V5PkpVCnnK0"]
+}
+fullAlbumDict = {album: {"tvAlbums": tvAlbums[album], "stolenAlbums": stolenAlbums[album]} for album in albumNames}
 
 
-fearless_stolen_albums = ["OLAK5uy_muzVJZB508O6Pn7hAnKLX-0FhxVFC9Z9M", "OLAK5uy_lZdZBuYMGZcc5AJZmJeGN-390ORcsEtJU"]
-
-
-
-def getTrackNames(search_results):
-    album = ytmusic.get_album(search_results)
+def getTrackNames(albumDict):
     albumsDict = {}
-    for trackNum in range(len(album["tracks"])):
-        trackTitle = (album["tracks"][trackNum]["title"])
-        tvVideoId = (album["tracks"][trackNum]["videoId"])
-    
-        if "[From The Vault]" not in trackTitle:
-            for k in trackTitle.split("\n"):
-                trackTitle = " ".join(re.findall(r"[a-zA-Z0-9]+", k))
-            albumsDict[trackTitle] = {}
-            albumsDict[trackTitle]["stolenVideoIds"] = []
-            albumsDict[trackTitle]["tvVideoId"] = tvVideoId
+
+    for albumName in albumDict:
+        albumsDict[albumName] = {}
+
+        # Combined processing for both TV and stolen albums
+        for albumType in ["tvAlbums", "stolenAlbums"]:
+            for albumId in albumDict[albumName][albumType]:
+                search_results = ytmusic.get_album_browse_id(albumId)
+                albumData = ytmusic.get_album(search_results)
+
+                for track in albumData["tracks"]:
+                    trackTitle = track["title"]
+
+                    # Skip tracks with "Voice Memo" or "Commentary"
+                    if "Voice Memo" in trackTitle or "Commentary" in trackTitle:
+                        continue
+
+                    if "From The Vault" not in trackTitle:
+                        # Clean and standardize the track title
+                        trackTitle = " ".join(re.findall(r"[a-zA-Z0-9\(\)\&]+", trackTitle))
+                        trackTitle = trackTitle.replace(" (Taylor s Version)", "").strip()
+
+                        # Initialize dictionary for the track if it does not exist
+                        if trackTitle not in albumsDict[albumName]:
+                            albumsDict[albumName][trackTitle] = {"stolenVideoIds": [], "tvVideoId": None}
+
+                        # Assign video ID based on album type
+                        videoIdKey = "tvVideoId" if albumType == "tvAlbums" else "stolenVideoIds"
+                        videoIdList = albumsDict[albumName][trackTitle][videoIdKey]
+                        if isinstance(videoIdList, list):
+                            videoIdList.append(track["videoId"])
+                        else:
+                            albumsDict[albumName][trackTitle][videoIdKey] = track["videoId"]
+
     return albumsDict
 
-# albumDict = {item: {} for item in getTrackNames(search_results)}
-# print(json.dumps(getTrackNames(search_results), indent=4))
 
-albumDict = getTrackNames(search_results)
-for trackName in albumDict.keys():
-    stolenTrackName = trackName.replace(" (Taylor’s Version)", "").strip()
-    
-    for albumPlaylistId in fearless_stolen_albums:
-        albumId = ytmusic.get_album_browse_id(albumPlaylistId)
-        album = ytmusic.get_album(albumId)
-        for trackNum in range(len(album["tracks"])):
-            if stolenTrackName.lower() == album["tracks"][trackNum]["title"].lower():
-                albumDict[trackName]["stolenVideoIds"].append(album["tracks"][trackNum]["videoId"])
 
-print(json.dumps(albumDict, indent=4))
-                
 
-# print(json.dumps(fearless["tracks"], indent=4))
-# Taylor Swift channel ID = UCqECaJ8Gagnn7YCbPEzWH6g
-# get_playlists = ytmusic.get_library_playlists()
-# album_info = ytmusic.get_artist("UCqECaJ8Gagnn7YCbPEzWH6g")
-# album_list = ytmusic.get_artist_albums("UCqECaJ8Gagnn7YCbPEzWH6g", params="ggMIegYIARoCAQI%3D")
+print(json.dumps(getTrackNames(fullAlbumDict), indent=4))
+# print(json.dumps(albumDict, indent=4))
